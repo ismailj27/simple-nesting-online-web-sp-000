fc35679117888ba47232fadfe9929279ad67fff7
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-31 18:34:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,5 +133,5 @@ def adding_to_dennis
         
         dennis_languages = programmer_hash[:dennis_ritchie][:languages]
         dennis_languages.push("Assembly")
-        dennis_languages[1].to_s
+        return dennis_languages[1]
 end

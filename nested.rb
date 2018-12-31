2d80ac4e5d3541c1ca54ee22a408dc71ce5eaa5c
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-31 18:30:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     programmer_hash[:alan_kay][:known_for].to_s
+     programmer_hash[:alan_kay][:known_for]
      
 end
 
